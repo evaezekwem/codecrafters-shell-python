74dd3f43c0b6ed18b9df37663a9c1cd84a006ea8
Evaristus Ezekwem <evaezekwem@gmail.com>
2025-09-01 17:02:30 -0400

    REPL implementation

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 import sys
+import random
 
 
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
 
-    # Wait for user input
-    command: str = input()
-    print(f"{command}: command not found")
+    try:
+        # Wait for user input
+        while True:
+            command: str = input()
+            print(f"{command}: command not found")
+            sys.stdout.write("$ ")
+    except KeyboardInterrupt as e:
+        print("\nExiting...")
+        sys.exit(0)
+    finally:
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
