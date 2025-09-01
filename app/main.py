3ef032414c9a5aafa824d7418118ca38d86a626c
Evaristus Ezekwem <evaezekwem@gmail.com>
2025-09-01 17:21:32 -0400

    Fixed exit code issues

--- app/main.py
+++ app/main.py
@@ -10,9 +10,14 @@ def main():
         # Wait for user input
         while True:
             command: str = input()
-            print(f"{command}: command not found")
-            sys.stdout.write("$ ")
-            exit(0)
+            
+            if command.strip() == "exit":
+                print("$ exit 0")
+                exit(0)
+            else:
+                print(f"{command}: command not found")
+                sys.stdout.write("$ ")
+            
     except KeyboardInterrupt as e:
         print("\nExiting...")
         sys.exit(0)
