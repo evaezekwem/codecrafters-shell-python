ee152619da527727436d55d37deee3147f40e008
Evaristus Ezekwem <evaezekwem@gmail.com>
2025-09-01 21:15:34 -0400

    Implemented type function

--- app/main.py
+++ app/main.py
@@ -5,16 +5,22 @@ import random
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
+    builtin_cmds = ["echo", "exit","type"]
 
     try:
         # Wait for user input
         while True:
             command: str = input()
+
+            # if command.strip().split()[0] in builtins_cmds:
             
             if command.strip() == "exit 0":
                 exit(0)
             elif command.strip().startswith("echo "):
                 print(command.strip()[5:])
+                sys.stdout.write("$ ")
+            elif command.strip().startswith("type "): #and command.strip().split()[1].strip in builtins_cmds:
+                print(f"{command.strip()[5:].partition(" ")[0]} is a shell builtin") if command.strip()[5:].partition(" ")[0] in builtin_cmds else print(f"{command.strip()[5:].partition(" ")[0]}: command not found")
                 sys.stdout.write("$ ")
             else:
                 print(f"{command}: command not found")
